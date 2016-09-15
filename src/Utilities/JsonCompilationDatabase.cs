e39bb91bc2c41a55da4208b23dae815d37daee7b
comicfans44 <comicfans44@gmail.com>
2016-09-15 16:46:26 +0800

    add JsonCompilationDatabase to do json save action

---
+++ src/Utilities/JsonCompilationDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Microsoft.Build.Utilities
+{
+    public class JsonCompilationDatabase
+    {
+        public static JsonCompilationDatabase Instance = new JsonCompilationDatabase();
+
+        private class Item
+        {
+            public string directory;
+            public string command;
+            public string file;
+        }
+
+        private List<Item> _items = new List<Item>();
+
+        public void Add(string dir,string cmd)
+        {
+            lock (this)
+            {
+                String[] tokens=cmd.Split(' ');
+
+                _items.Add(new Item {
+                    directory = dir,
+                    command = cmd,
+                    file = tokens[tokens.Length - 1]
+                });
+            }
+        }
+
+        public void Save()
+        {
+            lock (this)
+            {
+
+                System.IO.File.WriteAllText("msbuild_compile_commands.json",
+                    JsonConvert.SerializeObject(_items.ToArray(), Formatting.Indented));
+            }
+        }
+    }
+}
